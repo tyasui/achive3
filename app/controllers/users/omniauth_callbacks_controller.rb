c3ce05bb3641bcc2cb05b80e17adb604123002d8
Tsuyoshi Yasui <tyasui@nttpc.co.jp>
2017-05-27 13:52:36 +0000

    20170527_5

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
@@ -7,6 +8,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def twitter
+    # You need to implement the method below in your model
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
     end
   end
