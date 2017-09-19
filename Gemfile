a07c722d99ba657f2545d19995d4c9e13ca26f49
Tsuyoshi Yasui <tyasui@nttpc.co.jp>
2017-09-19 09:37:54 +0000

    “コミットの内容”

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ gem 'omniauth-facebook'
 gem 'carrierwave'
 gem 'rmagick'
 
+gem 'faker'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do |n|
+  name = Faker::HarryPotter.character
+  email = Faker::Internet.email
+  password = Faker::Internet.password
+  uid = SecureRandom.uuid
+  user = User.create(
+    name: name,
+    email: email,
+    password: password,
+    uid: uid
+  )
+
+  topic = Contribution.new(
+    content: 'トピック',
+  user_id: user.id)
+
+  10.times do
+    topic.comments.build(
+      content: 'コメント',
+    user_id: user.id)
+  end
+
+  topic.save
+
+end
+
+
