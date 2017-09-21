02152d7f52abc66c49402bf9d87ff6ba927f649f
Tsuyoshi Yasui <tyasui@nttpc.co.jp>
2017-09-21 09:34:18 +0000

    “コミットの内容”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-config.action_mailer.default_url_options = { host: 'achieve3.herokuapp.com' }
+config.action_mailer.default_url_options = { host: '52.69.244.207' }
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
